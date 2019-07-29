1a9b6aba71bc2c0f91209714e71384a06a64c525
luca <l@l.ro>
2019-07-29 23:18:41 +0300

    ds

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import csv
 import glob
+import os
 import sys
 import time
 import argparse
+import subprocess
 
 
 class bcolors:
@@ -12,10 +14,43 @@ class bcolors:
     ENDC = '\033[0m'
 
 
-parent_folder = 'Costal area'
+parent_folder = 'Costal area/'
 elevation_folder = 'Elevation'
 bathymetry_folder = 'Bathymetry'
-result_folder = 'Results'
+result_folder = 'Results/'
+
+input_files_elev = []
+input_files_bath = []
+
+path = ""
+input_folder_elev = ""
+input_folder_bath = ""
+
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', autosize=False):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        autosize    - Optional  : automatically resize the length of the progress bar to the terminal window (Bool)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    styling = '%s |%s| %s%% %s' % (prefix, fill, percent, suffix)
+    if autosize:
+        columns, stderr = subprocess.Popen("tput cols", stdout=subprocess.PIPE, stderr=None, shell=True).communicate()
+        length = int(columns) - len(styling)
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s' % styling.replace(fill, bar), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
 
 def create_arg_parser():
@@ -28,7 +63,7 @@ def create_arg_parser():
                         help='Path to the input directory.')
     parser.add_argument('--outputDirectory',
                         help='Path to the output that contains the resumes.')
-    parser.print_help()
+    # parser.print_help()
     try:
         options = parser.parse_args()
     except:
@@ -37,17 +72,19 @@ def create_arg_parser():
 
 
 def check_input_folders():
+    global path
     path = '/Users/lucian/PycharmProjects/mergeFiles/Costal area/'
 
-    input_folder_elev = path + '/' + elevation_folder
-    input_folder_bath = path + '/' + bathymetry_folder
+    global input_folder_elev, input_folder_bath
+    input_folder_elev = path + elevation_folder
+    input_folder_bath = path + bathymetry_folder
 
+    global input_files_elev, input_files_bath
     input_files_elev = [f for f in glob.glob(input_folder_elev + "**/*.csv", recursive=False)]
     input_files_bath = [f for f in glob.glob(input_folder_bath + "**/*.csv", recursive=False)]
     if len(input_files_elev) != len(input_files_bath):
         print(bcolors.FAIL + "Number of files in source folders is different. Stopping now" + bcolors.ENDC)
-
-    sys.exit(1)
+        sys.exit(1)
 
 
 def main():
@@ -55,17 +92,22 @@ def main():
 
     check_input_folders()
 
-    for x in range(1):
+    printProgressBar(0, len(input_files_elev), prefix='Progress:', suffix='Complete', length=50)
+
+
+    for x in range(len(input_files_elev)):
+        #print(os.path.basename(input_files_elev[x]))
+        printProgressBar(x + 1, len(input_files_elev), prefix='Progress:', suffix='Complete', length=50)
         elevList = []
         bathList = []
 
         resList = []
-        with open('Costal area/Elevation/S5_01_001&3952.csv', mode='r', encoding='utf-8-sig') as elev_csv_file:
+        with open(input_files_elev[x], mode='r', encoding='utf-8-sig') as elev_csv_file:
             csv_reader = csv.DictReader(elev_csv_file)
             for row in csv_reader:
                 elevList.append(row)
 
-        with open('Costal area/Bathymetry/S5_01_001&3952.csv', mode='r') as bath_csv_file:
+        with open(input_folder_bath+'/'+os.path.basename(input_files_elev[x]), mode='r') as bath_csv_file:
             csv_reader = csv.DictReader(bath_csv_file)
             for row in csv_reader:
                 bathList.append(row)
@@ -109,8 +151,7 @@ def main():
 
             resList[i] = result
 
-        filename = str(x) + 'resultfile.csv'
-        with open(parent_folder + '/' + result_folder + '/' + filename, mode='w') as csv_file:
+        with open(parent_folder + result_folder + os.path.basename(input_files_elev[x]), mode='w') as csv_file:
             fieldnames = ['CRS_ID', 'Measured_point_ID', 'X', 'Y', 'Z', 'Distance', 'Interval']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
@@ -122,6 +163,7 @@ def main():
 
 
 start = time.time()
+print('Starting doing magic')
 main()
 end = time.time()
 print(f'\nDuration of the execution was: {end - start}')
